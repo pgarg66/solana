88fe0d2c8e51068f2d09ee749c767703c45a29df
Pankaj Garg <pankaj@anza.xyz>
2025-03-05 08:06:36 -0800

    cleanup imports

--- compute-budget-instruction/src/compute_budget_instruction_details.rs
+++ compute-budget-instruction/src/compute_budget_instruction_details.rs
@@ -236,10 +236,6 @@ mod test {
         solana_instruction::Instruction,
         solana_keypair::Keypair,
         solana_message::Message,
-        solana_program_runtime::execution_budget::{
-            DEFAULT_INSTRUCTION_COMPUTE_UNIT_LIMIT, MAX_BUILTIN_ALLOCATION_COMPUTE_UNIT_LIMIT,
-            MAX_COMPUTE_UNIT_LIMIT, MAX_HEAP_FRAME_BYTES, MIN_HEAP_FRAME_BYTES,
-        },
         solana_pubkey::Pubkey,
         solana_signer::Signer,
         solana_svm_transaction::svm_message::SVMMessage,
